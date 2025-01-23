cf92ca13bae31e91a2d5eeb7b6b7420515485069
RiXXXhun <100133611+RiXXXhun@users.noreply.github.com>
2025-01-23 15:55:10 +0100

    aaa

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,42 +1,50 @@
-import { ITodo } from "@/app/todos/page";
-import { Button, Col, Form, Row } from "react-bootstrap";
+"use client"
 
-export default function AddTodo({
+import { ITodo } from "@/app/todos/page"
+import { init } from "next/dist/compiled/webpack/webpack";
+import { BaseSyntheticEvent, useState } from "react";
+import { Button, Card, Col, Row } from "react-bootstrap"
+
+export default function TodosDisplay({
     todos,
     setTodos
 } : {
     todos: ITodo[]
     setTodos: Function
 }) {
+
+    function handleSubmit(event: BaseSyntheticEvent) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const formValues = Object.fromEntries(formData)
+
+        fetch("https://jsonplaceholder.typicode.com/todos", {
+            method: 'POST',
+            body: JSON.stringify({
+                ...formValues
+            }),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        }).then((response) => {
+            console.log(response)
+        })
+    }
+
     return (
         <Col xs={12}>
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <Row>
-                    <Col xs={3}>
-                        <Form.Group className="mb-3">
-                            <Form.Label> Cím </Form.Label>
-                            <Form.Control type="text" name="title" required/>      
-                        </Form.Group>
+                {todos.map((todo) => (
+                    <Col key={todo.id} xs={3}>
+                        <Card>
+                            <Card.Title>{todo.title}</Card.Title>
+                            <Card.Text>
+                                {todo.completed ? 'Készen van' : 'Nincsen készen'}
+                            </Card.Text>
+                        </Card>
                     </Col>
-                    <Col xs={3}>
-                        <Form.Group className="mb-3">
-                            <Form.Label> Felhasználó azonosító </Form.Label>
-                            <Form.Control type="number" name="userId" required/>      
-                        </Form.Group>
-                    </Col>
-                    <Col xs={3}>
-                        <Form.Group className="mb-3">
-                            <Form.Label> Készen van ? </Form.Label>
-                            <Form.Check 
-                                type="switch"
-                                name="completed"
-                                
-                            />
-                        </Form.Group>
-                    </Col>
-                    <Col xs={3}>
-                        <Button variant="success" type="submit"> Mentés </Button>
-                    </Col>
+                ))}
                 </Row>
             </form>
         </Col>
--- src/components/TodosDisplay.tsx
+++ src/components/TodosDisplay.tsx
@@ -1,5 +1,5 @@
-import { ITodo } from "@/app/todos/page"
-import { Col } from "react-bootstrap"
+import { ITodo } from "@/app/todos/page";
+import { Card, Col, Row } from "react-bootstrap";
 
 export default function TodosDisplay({
     todos,
@@ -10,7 +10,18 @@ export default function TodosDisplay({
 }) {
     return (
         <Col xs={12}>
-        
+            <Row>
+                {todos.map((todo) => (
+                    <Col key={todo.id} xs={3}>
+                        <Card>
+                            <Card.Title>{todo.title}</Card.Title>
+                            <Card.Text>
+                                {todo.completed ? 'Készen van' : 'Nincsen készen'}
+                            </Card.Text>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
         </Col>
     )
 }
